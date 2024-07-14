ee69b09e560ee43a34a0a49fa501fb3ffa4d4ab1
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-07-14 10:01:53 -0700

    Currency Lab

--- src/main/java/com/cydeo/exception/ExceptionWrapper.java
+++ src/main/java/com/cydeo/exception/ExceptionWrapper.java
@@ -1,17 +1,24 @@
 package com.cydeo.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.List;
+
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionWrapper {
 
     private LocalDateTime timestamp;
     private Integer status;
     private String message;
     private String path;
+
+    private Integer errorCount;
+    private List<ValidationException> validationExceptionList;
 
     public ExceptionWrapper(Integer status, String message, String path) {
         this.status = status;
