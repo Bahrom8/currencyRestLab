41d66023c314f35dfbbef49678165a0d53e16c8b
bakhromizzatullaev <bakhromizzatulife@gmail.com>
2024-07-14 09:06:05 -0700

    Currency Lab

---
+++ src/main/java/com/cydeo/exception/ExceptionWrapper.java
@@ -0,0 +1,26 @@
+package com.cydeo.exception;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+
+public class ExceptionWrapper {
+
+    private LocalDateTime timestamp;
+    private Integer status;
+    private String message;
+    private String path;
+
+    public ExceptionWrapper(Integer status, String message, String path) {
+
+        this.status = status;
+        this.message = message;
+        this.path = path;
+        this.timestamp = LocalDateTime.now();
+
+    }
+}
